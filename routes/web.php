fcf556919a04f1d7778cb7e3926b460bf274d2d1
chihiro-user <kafe824.chihiro@gmail.com>
2024-10-27 08:42:24 +0000

    Laravel13課題

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,14 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
 
 //Laravei09 課題4
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/create', 'add')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
 });
 
-Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
 Auth::routes();
 
