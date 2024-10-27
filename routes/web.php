4a878b592e5cb45ddd5ba7a5ebc1d3429428ddba
chihiro-user <kafe824.chihiro@gmail.com>
2024-10-27 07:49:27 +0000

    Laravel13

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
 //Laravei09 課題4
