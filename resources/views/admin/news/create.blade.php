e7b8f551e4f7ca39249d4fd6fc8666593316de5b
chihiro-user <kafe824.chihiro@gmail.com>
2024-11-18 10:41:06 +0000

    AWS

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>ニュースを新規作成する</h2>
                 <form action="{{ route('admin.news.create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
